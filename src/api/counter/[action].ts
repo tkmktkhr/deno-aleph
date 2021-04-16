51aca27ad2d2067f17bcb7f52e0729704035e792
tkmktkhr <mukku.wordpress@gmail.com>
2021-04-16 09:55:31 +0900

    refactor: formatting

--- src/api/counter/[action].ts
+++ src/api/counter/[action].ts
@@ -1,27 +1,27 @@
-import type { APIRequest } from 'aleph/types.ts'
+import type { APIRequest } from "aleph/types.ts";
 
-const store = globalThis as any
+const store = globalThis as any;
 
 export default async function handler(req: APIRequest) {
-  let count = store.$count || 0
+  let count = store.$count || 0;
 
-  switch (req.params['action']) {
-    case 'increase':
-      count++
-      store.$count = count
-      req.json({ count })
-      break
-    case 'decrease':
-      count--
-      store.$count = count
-      req.json({ count })
-      break
+  switch (req.params["action"]) {
+    case "increase":
+      count++;
+      store.$count = count;
+      req.json({ count });
+      break;
+    case "decrease":
+      count--;
+      store.$count = count;
+      req.json({ count });
+      break;
     default:
       req.status(400).json({
-        error: 'UnknownAction',
+        error: "UnknownAction",
         status: 400,
-        message: `undefined action '${req.params['action']}'`
-      })
-      break
+        message: `undefined action '${req.params["action"]}'`,
+      });
+      break;
   }
 }
