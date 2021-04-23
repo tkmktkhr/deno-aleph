8403f3317cb9520b5c3ddc039c25b3f95927ed9d
tkmktkhr <mukku.wordpress@gmail.com>
2021-04-23 13:41:22 +0900

    fea: create 500 page

---
+++ src/pages/E500.tsx
@@ -0,0 +1,17 @@
+import Logo from "../components/logo.tsx";
+import React from "react";
+
+
+export default function E500() {
+  console.log('insinde of Error 500');
+  return (
+    <div className="page">
+      <p className="logo">
+        <Logo />
+      </p>
+      <h1>
+        Welcome to <strong>500 ERROR</strong>!
+      </h1>
+    </div>
+  );
+};
